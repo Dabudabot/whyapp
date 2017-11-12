a59faa90dc7cc440ba02ec201b7c2d785af40b00
Daulet Tumbayev <daulettbot@gmail.com>
2017-11-12 23:17:12 +0300

    forward ready

--- app/src/main/java/com/inno/dabudabot/whyapp/ui/dialogs/MessagingDialog.java
+++ app/src/main/java/com/inno/dabudabot/whyapp/ui/dialogs/MessagingDialog.java
@@ -1,34 +1,49 @@
 package com.inno.dabudabot.whyapp.ui.dialogs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.inno.dabudabot.whyapp.R;
-import com.inno.dabudabot.whyapp.ui.activities.UserChooserActivity;
+import com.inno.dabudabot.whyapp.controller.ReceiveContentController;
+import com.inno.dabudabot.whyapp.controller.SendContentController;
+import com.inno.dabudabot.whyapp.controller.listing.GetListingsController;
+import com.inno.dabudabot.whyapp.listener.GetListingsView;
+import com.inno.dabudabot.whyapp.listener.SendContentView;
 import com.inno.dabudabot.whyapp.wrappers.DeleteContentWrapper;
 import com.inno.dabudabot.whyapp.wrappers.ForwardWrapper;
 import com.inno.dabudabot.whyapp.wrappers.RemoveContentWrapper;
 
+import java.util.List;
+
+import Util.Constants;
 import Util.Settings;
 import eventb_prelude.BRelation;
+import eventb_prelude.BSet;
 import eventb_prelude.Pair;
+import group_6_model_sequential.Content;
+import group_6_model_sequential.User;
 
 /**
  * Created by dabudabot on 12.11.17.
  */
 
 public class MessagingDialog extends Dialog implements
-        android.view.View.OnClickListener {
+        android.view.View.OnClickListener, GetListingsView, SendContentView {
 
     private Activity c;
-    private Dialog d;
     private Integer contentId;
     private Integer userId;
     private Integer i;
+    private GetListingsController getListingsController;
+    private SendContentController sendContentController;
+    private BSet<Integer> userIds;
 
     public MessagingDialog(Activity a, Integer contentId) {
         super(a);
@@ -62,6 +77,9 @@ public class MessagingDialog extends Dialog implements
         Button delete = (Button) findViewById(R.id.delete_msg);
         forward.setOnClickListener(this);
         delete.setOnClickListener(this);
+        getListingsController = new GetListingsController(this);
+        sendContentController = new SendContentController(this);
+        userIds = new BSet<>();
     }
 
     @Override
@@ -69,7 +87,7 @@ public class MessagingDialog extends Dialog implements
         switch (v.getId()) {
             case R.id.forward:
                 Settings.getInstance().setForwardingContent(contentId);
-                UserChooserActivity.startActivity(getContext());
+                getListingsController.getChatsListing();
                 break;
             case R.id.delete_msg:
                 if (Settings.getInstance()
@@ -79,13 +97,15 @@ public class MessagingDialog extends Dialog implements
                             Settings.getInstance().getMachine().get_active()) {
                         snd = pair.snd();
                     }
-                    
                     new RemoveContentWrapper()
                             .runRemoveContent(contentId,
                                     Settings.getInstance().getCurrentUser().getId(),
                                     snd,
                                     i,
                                     Settings.getInstance().getMachine());
+                    ReceiveContentController.sendNotify(
+                            Settings.getInstance().getCurrentUser().getId(),
+                            snd);
                 } else {
                     new DeleteContentWrapper()
                             .runDeleteContent(contentId,
@@ -93,12 +113,104 @@ public class MessagingDialog extends Dialog implements
                                     userId,
                                     i,
                                     Settings.getInstance().getMachine());
+                    ReceiveContentController.sendNotify(
+                            Settings.getInstance().getCurrentUser().getId(),
+                            userId);
                 }
-
                 break;
             default:
                 break;
         }
         dismiss();
+    }
+
+    @Override
+    public void onGetListingsSuccess(List<User> users) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        final List<User> userList = users;
+        String[] names = new String[users.size()];
+        final boolean[] checkedNames = new boolean[users.size()];
+        int i = 0;
+        for (User user : userList) {
+            names[i] = user.getName();
+            checkedNames[i] = false;
+            i++;
+        }
+
+        builder.setMultiChoiceItems(names, checkedNames,
+                new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                // Update the current focused item's checked status
+                checkedNames[which] = isChecked;
+            }
+        });
+
+        // Specify the dialog is not cancelable
+        builder.setCancelable(false);
+
+        // Set a title for alert dialog
+        builder.setTitle("User chooser");
+
+        // Set the positive/yes button click listener
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do something when click positive button
+                Integer forwardingContent = Settings.getInstance().getForwardingContent();
+                Settings.getInstance().setForwardingContent(null);
+
+                for (int i = 0; i < checkedNames.length; i++) {
+                    if (checkedNames[i]) {
+                        userIds.add(userList.get(i).getId());
+                    }
+                }
+
+                sendContentController.send(
+                        Settings.getInstance()
+                                .getContents()
+                                .get(forwardingContent)
+                                .getMessage(),
+                        getOwnerActivity());
+            }
+        });
+
+        // Set the neutral/cancel button click listener
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Settings.getInstance().setForwardingContent(null);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        // Display the alert dialog on interface
+        dialog.show();
+    }
+
+    @Override
+    public void onGetListingsFailure(String message) {
+
+    }
+
+    @Override
+    public void sendSuccess(Content content) {
+        new ForwardWrapper().runForward(
+                content.getId(),
+                Settings.getInstance().getCurrentUser().getId(),
+                userIds,
+                Settings.getInstance().getMachine());
+        FirebaseDatabase.getInstance()
+                .getReference()
+                .child(Constants.NODE_CONTENTS)
+                .child(content.getId().toString())
+                .setValue(content);
+    }
+
+    @Override
+    public void sendFailure(String message) {
+
     }
 }
